7724c9f4582ee8ba25d7c34c78e175ebe41f2af9
Megharth <megharth.lakhataria@alian.aliansoftware.org>
2019-01-30 17:50:46 +0530

    init

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-
+import KsVueScrollmagic from 'ks-vue-scrollmagic'
 Vue.config.productionTip = false
 
+Vue.use(KsVueScrollmagic)
 new Vue({
   router,
   store,
---
+++ src/views/Main.vue
@@ -0,0 +1,74 @@
+<template>
+    <div id="main">
+        <section>
+           <div id="landing">
+               <div class="pin-scene">
+                   <div class="heading"><h1>Some Text</h1></div>
+                   <div class="bg-image"></div>
+               </div>
+           </div>
+        </section>
+        <section>
+            <div id="about"></div>
+        </section>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Main",
+    mounted() {
+      let t1 = new this.$gsap.TimelineMax()
+      t1.from('.bg-image', 0.5, {
+        scale: 1.5,
+        autoAlpha: 0,
+        ease: Power1.easeOut
+      })
+
+      let controller = new this.$scrollmagic.Controller()
+
+      //LANDING COMPONENT SCROLL
+      let landingScrollTl = new this.$gsap.TimelineMax().add([
+        TweenMax.to('.heading', 4, {
+          autoAlpha: 0,
+          scale: 2,
+          y: 100
+        }),
+        TweenMax.to('.bg-image', 4, {
+          scale: 1.5
+        })
+      ])
+
+      let scrollLanding = new this.$scrollmagic.Scene({
+        triggerElement: '.pin-scene',
+        triggerHook: 0,
+        duration: '120%'
+      })
+        .setTween(landingScrollTl)
+        .setPin('.pin-scene')
+        .addTo(controller)
+
+    //  ABOUT COMPONENT SCROLL
+
+      let aboutScrollTl = new this.$gsap.TimelineMax().add([
+        TweenMax.to('.bg-image', 4, {
+          autoAlpha: 0
+        })
+      ])
+
+      let scrollAbout = new this.$scrollmagic.Scene({
+        triggerHook: 0,
+        triggerElement: '#about',
+        duration: '30%'
+      })
+        .setTween(aboutScrollTl)
+        .setPin('#about')
+        .addTo(controller)
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+    @import ../sass/main
+</style>
