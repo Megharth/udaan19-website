4ce755f5ca62c815c12ae78267221a387963e7ad
Manav13 <manavprajapati1398@gmail.com>
2019-02-06 22:45:04 +0530

    init

---
+++ src/views/buildersOfAzkaban.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="buidersOfAzkaban">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "buildersOfAzkaban"
+    }
+</script>
+
+<style scoped lang="sass">
+body
+    background: url("../assets/Azkaban_concept_art.png") no-repeat
+</style>
