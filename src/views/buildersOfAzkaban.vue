bf768346e697a6e7076da3ec1fe2b41f16d5671f
Manav13 <manavprajapati1398@gmail.com>
2019-02-07 00:20:29 +0530

    Added Background immage

--- src/views/buildersOfAzkaban.vue
+++ src/views/buildersOfAzkaban.vue
@@ -1,5 +1,10 @@
 <template>
     <div id="buidersOfAzkaban">
+        <!--<div class="card" style="width: 18rem;">-->
+            <!--<div id="cardBorder">-->
+                <!--<span id="eventName">event name</span>-->
+            <!--</div>-->
+        <!--</div>-->
     </div>
 </template>
 
@@ -10,6 +15,32 @@
 </script>
 
 <style scoped lang="sass">
-body
-    background: url("../assets/Azkaban_concept_art.png") no-repeat
+#buidersOfAzkaban
+    position: absolute
+    width: 100%
+    height: 100%
+    background: url("../assets/Azkaban_concept_art.png") no-repeat center center
+    background-size: cover
+/*.card*/
+    /*top: 25%*/
+    /*left: 20%*/
+    /*width: 50%*/
+    /*height: 50%*/
+    /*background: #F6E277*/
+    /*opacity: 0.7*/
+    /*margin: 0 0*/
+/*#cardBorder*/
+    /*position: absolute*/
+    /*top: 5%*/
+    /*left: 4%*/
+    /*width: 90%*/
+    /*height: 90%*/
+    /*border: black 3px solid*/
+    /*text-align: center*/
+/*#eventName*/
+    /*position: relative*/
+    /*top: 40%*/
+    /*text-transform: uppercase*/
+    /*font-weight: 700*/
+    /*font-size: 1.4rem*/
 </style>
