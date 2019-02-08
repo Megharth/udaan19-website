4f93d74d4546c9aacefdf7dae8798f677be606c9
Megharth <megharth.lakhataria@alian.aliansoftware.org>
2019-02-08 17:24:18 +0530

    add hlafWavePrince

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from './views/Main'
-
+import halfWavePrince from './views/halfWavePrince'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'main',
       component: Main
+    },
+    {
+      path: '/halfWavePrince',
+      name: 'halfWavePrince',
+      component: halfWavePrince
     }
   ]
 })
