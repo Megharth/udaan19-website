9cfbb0e5bc221f74cd412cc5a5b188da6b09c0e5
Megharth <megharth.lakhataria@alian.aliansoftware.org>
2019-02-11 17:56:28 +0530

    add goblet-of-entertainment

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Main from './views/Main'
 import halfWavePrince from './views/halfWavePrince'
+import gobletOfEntertainment from './views/gobletOfEntertainment'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/halfWavePrince',
       name: 'halfWavePrince',
       component: halfWavePrince
+    },
+    {
+      path: '/gobletOfEntertainment',
+      name: 'gobletOfEntertainment',
+      component: gobletOfEntertainment
     }
   ]
 })
---
+++ src/views/gobletOfEntertainment.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="gobletOfEntertainment">
+        <div class="goblet">
+             <div class="flame">
+                <span v-for="n in 100" :style="{'--n':n, '--rnd': Math.random()}"></span>
+            </div>
+            <img src="../assets/goblet.png" alt="goblet" class="img-fluid">
+        </div>
+        <div class="events">
+            <div class="event">Some text</div>
+            <div class="event">some Text</div>
+            <div class="event">some text</div>
+            <div class="event">Some Text</div>
+            <div class="event">Some Text</div>
+            <div class="event">Some Text</div>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "gobletOfEntertainment",
+    mounted() {
+      let t1 = new this.$gsap.TimelineMax()
+      t1.staggerFrom('.event', 2, {
+        // y: -1000,
+        autoAlpha: 0,
+        scale: 0
+      }, 0.1)
+      t1.staggerTo('.event', 1, {
+        cycle: {
+          rotation: function() {
+            return Math.random() * 45 - 20
+          }
+        },
+        autoAlpha: 1
+      }, 0.1, "-=1.2")
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+@import ../sass/gobletOfEntertainment
+
+</style>
