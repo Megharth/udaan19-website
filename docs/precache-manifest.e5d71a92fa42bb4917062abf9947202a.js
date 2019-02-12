024762a1393e220cab661f2351d875905de39939
Megharth <m.c.lakhataria@gmail.com>
2019-02-12 12:11:37 +0530

    coming soon push

---
+++ docs/precache-manifest.e5d71a92fa42bb4917062abf9947202a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "75673e5ff5e07bee6c13b0f60fd694b2",
+    "url": "/fonts/dum1.75673e5f.ttf"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "13b93a56e46bc078b56c",
+    "url": "/js/chunk-vendors.86ed6ce7.js"
+  },
+  {
+    "revision": "75f7ce7cb558729ce66b",
+    "url": "/js/app.c2ac0b5d.js"
+  },
+  {
+    "revision": "5fbc441ad0e783bf4b085a10f996c6bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a82523f098927d147e36bcac40c5c36",
+    "url": "/img/logo.8a82523f.png"
+  },
+  {
+    "revision": "d98f58da5e961e4cfdfb2a2aa64971ea",
+    "url": "/img/instagram.d98f58da.png"
+  },
+  {
+    "revision": "1c61919a868476117a3c425b8ee82e3b",
+    "url": "/img/background.1c61919a.jpg"
+  },
+  {
+    "revision": "de2fa53b146ccc47bf1b93115f6b36f2",
+    "url": "/fonts/muggleNews.de2fa53b.ttf"
+  },
+  {
+    "revision": "1db8c3a84e1e1b78a1858360922bef15",
+    "url": "/fonts/headlineOne.1db8c3a8.TTF"
+  },
+  {
+    "revision": "13b93a56e46bc078b56c",
+    "url": "/css/chunk-vendors.3259982a.css"
+  },
+  {
+    "revision": "75f7ce7cb558729ce66b",
+    "url": "/css/app.25b50079.css"
+  }
+];
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -5,7 +5,7 @@
                 <div class="pin-scene">
                     <div class="bg-image"></div>
                     <div class="udaan-logo">
-                        <img src="../assets/logo.svg" alt="udaanLogo" class="img-fluid">
+                        <img src="../assets/logo.png" alt="udaanLogo" class="img-fluid">
                     </div>
                     <!--<div class="quidditch-ball"></div>-->
                     <!--<div class="demantadors">-->
@@ -18,17 +18,18 @@
                     <!--</ul>-->
                     <!--</div>-->
                     <!--</div>-->
+                    <div class="coming-soon">Coming soon</div>
                 </div>
             </div>
         </section>
-        <section>
-            <div id="about">
-                <aboutComponent class="aboutComponent"></aboutComponent>
-                <div class="particles">
-                    <div class="particle" v-for="n in 50"></div>
-                </div>
-            </div>
-        </section>
+        <!--<section>-->
+            <!--<div id="about">-->
+                <!--<aboutComponent class="aboutComponent"></aboutComponent>-->
+                <!--<div class="particles">-->
+                    <!--<div class="particle" v-for="n in 50"></div>-->
+                <!--</div>-->
+            <!--</div>-->
+        <!--</section>-->
     </div>
 </template>
 
@@ -41,6 +42,42 @@
       aboutComponent
     },
     mounted() {
+
+      //    COMING SOON ANIMATION
+
+      let t2 = new this.$gsap.TimelineMax({
+        repeat: -1,
+        yoyo: true
+      })
+
+      let line = document.querySelector('.coming-soon')
+      let letters = line.innerHTML.split("")
+      let blockLetters = []
+      letters.forEach((letter) => {
+        if(letter === " ")
+          blockLetters.push("<div class=\"letter\">&thinsp;</div>")
+        else
+          blockLetters.push("<div class=\"letter\">" + letter + "</div>")
+      })
+      line.innerHTML = blockLetters.join("")
+
+      document.querySelectorAll(".letter").forEach(function(el) {
+        el.style.display = "inline-block"
+      })
+
+      t2.staggerTo(".letter", 1.5, {
+        scale: 1.2,
+        y: -10
+      }, 0.1)
+
+      t2.staggerTo(".letter", 1, {
+        scale: 1,
+        y: 0
+      }, 0.1, 0)
+
+
+
+      //LANDING COMPONENT ANIMATIONS
       let t1 = new this.$gsap.TimelineMax()
       t1.from('.bg-image', 0.5, {
         scale: 1.5,
@@ -57,7 +94,7 @@
           scale: 2,
           y: 100
         }),
-        TweenMax.to('.bg-image', 4, {
+        TweenMax.to('.bg-image, .coming-soon', 4, {
           scale: 1.5
         }),
         TweenMax.to('.demantador', 4, {
@@ -83,7 +120,7 @@
 
       //  ABOUT COMPONENT SCROLL
 
-      let aboutScrollTl = new this.$gsap.TimelineMax().add([
+      /*let aboutScrollTl = new this.$gsap.TimelineMax().add([
         TweenMax.to('.bg-image', 4, {
           autoAlpha: 0
         }),
@@ -142,11 +179,33 @@
       })
         .setTween(aboutScrollTl)
         .setPin('#about')
-        .addTo(controller)
+        .addTo(controller)*/
+
+
+
     }
   }
 </script>
 
 <style scoped lang="sass">
     @import ../sass/main
+    #main
+        overflow: hidden
+
+    @font-face
+        font-family: 'dumbledore'
+        src: url("../assets/dum1.ttf")
+
+    .coming-soon
+        position: absolute
+        top: 30px
+        left: 50%
+        z-index: 3
+        transform: translateX(-50%)
+        color: black
+        text-align: center
+        font-family: dumbledore, sans-serif
+        font-size: 72px
+        .letter
+            display: inline-block !important
 </style>
