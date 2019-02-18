5d88ea5cba4421a4badd01e5928eb7dcf7170338
Megharth <megharth.lakhataria@alian.aliansoftware.org>
2019-02-18 14:22:48 +0530

    finalize builders-of-azkaban

--- src/views/buildersOfAzkaban.vue
+++ src/views/buildersOfAzkaban.vue
@@ -4,35 +4,100 @@
         <div class="heading">
             <p>Welcome to <br>Builders of Azkaban</p>
         </div>
-        <div class="eventTitle">
-            <div>Events</div>
+        <div class="events">
+            <div class="event" v-for="event in deptEvents" @click="eventsDetail = event">{{ event.eventName }}</div>
         </div>
-        <div class="eventNames">
-            <p>
-                <a href="#">Event 1</a>
-                <a href="#">Event 2</a><br>
-                <a href="#">Event 3</a>
-                <a href="#">Event 4</a>
-            </p>
-        </div>
-        <b-modal id="modal1" title="Bootstrap-Vue">
-            <p class="my-4">Hello from modal!</p>
-        </b-modal>
+
+        <transition name="fade">
+            <div class="eventDescription" v-if="eventsDetail">
+                <div class="eventName">{{ eventsDetail.eventName }}</div>
+                <div class="tagline">{{ eventsDetail.tagline }}</div>
+                <div class="event-content">
+                    <div class="rounds">
+                        <div class="round" v-for="(round, index) in eventsDetail.rounds"><span class="roundIndice">Round {{index + 1}} : </span>{{ round}}</div>
+                    </div>
+                    <div class="managers">
+                        <div class="manager" v-for="manager in eventsDetail.managers">
+                            <div class="name">{{manager.name}}</div>
+                            <div class="contact">{{manager.phone}}</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="warning">
+                    <div>Approach With Extreme Caution!</div>
+                    <div>Do not attempt to use magic in this event</div>
+                </div>
+                <div class="back" @click="eventsDetail = null">Back</div>
+            </div>
+        </transition>
     </div>
 
 </template>
 
 <script>
-    export default {
-        name: "buildersOfAzkaban",
-        mounted() {
-            let tl = new this.$gsap.TimelineMax()
+  import events from '../events'
+
+  export default {
+    name: "buildersOfAzkaban",
+    data() {
+      return {
+        events,
+        deptEvents: [],
+        eventsDetail: null
+      }
+    },
+    mounted() {
+
+      let t2 = new this.$gsap.TimelineMax()
+
+      t2.to('.events', 0.5, {
+        autoAlpha: 1
+      }, "+=2")
+
+      let tl = new this.$gsap.TimelineMax({
+        repeat: -1,
+        yoyo: true
+      })
+
+      Array.from(document.querySelector('.events').children, (event) => {
+        let letters = event.innerHTML.split("")
+        let blockLetters = []
+        letters.forEach((letter) => {
+          if (letter === " ")
+            blockLetters.push("<div class=\"letter\"> &nbsp; </div>")
+          else
+            blockLetters.push("<div class=\"letter\">" + letter + "</div>")
+        })
+        event.innerHTML = blockLetters.join("")
+      })
+
+      let letters = document.querySelectorAll('.letter')
+      letters.forEach(function (letter) {
+        letter.style.display = "inline-block"
+      })
+
+      tl.staggerTo(".letter", 0.5, {
+        scale: 1.2
+      }, 0.1)
+
+      tl.staggerTo(".letter", 0.5, {
+        scale: 1
+      }, 0.1, 0)
 
 
-        }
+
+
+    },
+    created() {
+      let self = this
+      this.events.forEach(function (event) {
+        if (event.department === "civil")
+          self.deptEvents.push(event)
+      })
     }
+  }
 </script>
 
 <style scoped lang="sass">
-@import ../sass/buildersOfAzkaban
+    @import ../sass/buildersOfAzkaban
 </style>
