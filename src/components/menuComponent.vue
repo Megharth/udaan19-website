6de22445b1bde0204365d820c6b2b438a1f3d1bd
Megharth <m.c.lakhataria@gmail.com>
2019-02-21 18:01:08 +0530

    change menu component

--- src/components/menuComponent.vue
+++ src/components/menuComponent.vue
@@ -1,9 +1,12 @@
 <template>
     <div id="menu">
-        <div class="icon">
+        <div class="icon" @click="openMenu()">
             <div class="line"></div>
             <div class="line"></div>
             <div class="line"></div>
+        </div>
+        <div class="tabs" v-if="isOpen">
+            <div class="tab" v-for="navigation in navigations" @click="$router.push(navigation.url)">{{ navigation.name }}</div>
         </div>
     </div>
 </template>
@@ -13,7 +16,58 @@
     name: "menuComponent",
     data() {
       return {
-        isOpen: false
+        isOpen: false,
+        navigations: [
+          {
+            name: 'Home',
+            url: '/'
+          },
+          {
+            name: "Builders Of Azkaban",
+            url: 'boa'
+          },
+          {
+            name: "Automotive Philosophers",
+            url: 'automotivePhilosophers'
+          },
+          {
+            name: "Chamber of Coders",
+            url: 'chamberOfCoders'
+          },
+          {
+            name: "Half Wave Prince",
+            url: 'halfWavePrince'
+          },
+          {
+            name: "M. A. D. Hollows",
+            url: 'madHollows'
+          },
+          {
+            name: "Scamander\'s Suitcase",
+            url: 'scamandersSuitcase'
+          },
+          {
+            name: "Order of Ohms",
+            url: 'orderOfOhms'
+          }
+        ]
+      }
+    },
+    methods: {
+      openMenu() {
+        let tl = new this.$gsap.TimelineMax({
+          paused: true
+        })
+        tl.to('#menu', 0.5, {
+          x: 0,
+          background: "#333333",
+          boxShadow: "-4px 0 4px rgba(0,0,0,0.25)"
+        })
+        if(this.isOpen)
+          tl.reverse()
+        else
+          tl.play()
+        this.isOpen = !this.isOpen
       }
     }
   }
